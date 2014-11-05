2d87769889fc09d8f981d8e6f8dcdd9efbb0053d
maxint <lnychina@gmail.com>
2014-11-05 18:20:02 +0800

    Add form saving and custom css

--- docserver/doc-server-project-ms.user.js
+++ docserver/doc-server-project-ms.user.js
@@ -1,25 +1,32 @@
 ﻿// ==UserScript==
 // @name        ArcSoft Project Management
-// @version     0.0.1
+// @version     0.0.2
 // @author      maxint <NOT_SPAM_lnychina@gmail.com>
 // @namespace   http://maxint.github.io
 // @description An enhancement for Arcsoft project management system in http://doc-server
 // @include     http://doc-server/*
 // @updateURL   https://github.com/maxint/userjs/docserver/doc-server-project-ms.user.js
 // @downloadURL https://github.com/maxint/userjs/docserver/doc-server-project-ms.user.js
-// @Note        Features:
-//              - 首页 | “项目” 导航到 “项目跟踪”
-//              - “项目跟踪”页面添加 "Release"
-//              - "Add Release Package" 窗口：自动填写日期，根据Release
-//                Package填写Version，自动查询Related Project名称。
+// @Note
+// v0.0.2
+//  - "Add Rlease" 窗口自动保存表单
+//
+// v0.0.1
+//  - 首页 | “项目” 导航到 “项目跟踪”
+//  - “项目跟踪”页面添加 "Release"
+//  - "Add Release Package" 窗口：
+//      * 自动填写日期
+//      * 根据Release Package填写Version
+//      * 自动查询Related Project名称
+//
 // ==/UserScript==
 
 // a function that loads jQuery and calls a callback function when jQuery has finished loading
-function withjQuery(callback, safe){
+function withjQuery(callback, safe) {
     if (typeof(jQuery) == "undefined") {
         var script = document.createElement("script");
         script.type = "text/javascript";
-        script.src = "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js";
+        script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js";
         if (safe) {
             var cb = document.createElement("script");
             cb.type = "text/javascript";
@@ -42,13 +49,21 @@ function withjQuery(callback, safe){
         setTimeout(function() {
             //Firefox supports
             console.log('Runing custom script');
-            callback(jQuery, typeof unsafeWindow === "undefined" ? window : unsafeWindow);
+            callback(jQuery, typeof(unsafeWindow) == "undefined" ? window : unsafeWindow);
         }, 30);
     }
 }
 
 // the guts of this userscript
 withjQuery(function($, window) {
+    // helper functions
+    function deserialize(name, def) {
+        return localStorage.getItem(name) || def;
+    }
+    function serialize(name, val) {
+        localStorage.setItem(name, val);
+    }
+
     // replace main menu [Project]
     $('li#mainMenuItem_150000 a').attr('href', '/projectManage/ProjectList.asp');
 
@@ -66,12 +81,10 @@ withjQuery(function($, window) {
         });
     } else if (subpath == '/projectManage/ProjectOther/addRelease.asp') {
         console.log('addRelease');
-        var tds = $('table.EditTable tbody tr td');
         // date
         var d = new Date();
-        var dstr = d.getFullYear() + '/' + d.getMonth() + '/' + d.getDay()
-        tds.eq(1).find('input:first').val(dstr);
-        // pkg input
+        $('input#txtDate').val(d.getFullYear() + '/' + d.getMonth() + '/' + d.getDay());
+        // release package
         $('input#txtDeliveryPackage').change(function(){
             var val = $(this).val();
             var vers = /(\d+)\.(\d+)\.\d+\.(\d+)/g.exec(val);
@@ -79,20 +92,43 @@ withjQuery(function($, window) {
                 vers = /(\d+)\.(\d+)\.(\d+)/g.exec(val);
             }
             if (vers) {
-                tds.eq(2).find('input:first').val('v' + [vers[1], vers[2], vers[3]].join('.'));
+                $("input[name='txtVersion']").val('v' + [vers[1], vers[2], vers[3]].join('.'));
             }
+        }).val(deserialize('release.package', '')).css({
+            'width': '100%',
         });
-        tds.eq(5).find('input:first').keyup(function(){
-            var val = $(this).val();
-            if (/^[0-9]{4}$/.test(val)) {
-                var url = '../ProjectDelivery/delivery_releated_project_list.asp?projid=' + val;
-                tds.eq(5).find('div.info').load(url + ' td:nth-child(2)');
+        $('input#txtReleasePath').val(deserialize('release.releasepath')).css({
+            'width': '100%',
+        });
+        $("input[name='txtVersion']").val(deserialize('release.version'));
+        // related project
+        function updateProjectName(id) {
+            var msgdiv = $('#btnCheckReleatedProject').next();
+            if (/^[0-9]{4}$/.test(id)) {
+                var url = '../ProjectDelivery/delivery_releated_project_list.asp?projid=' + id;
+                msgdiv.load(url + ' td:nth-child(2)');
             } else {
-                tds.eq(5).find('div.info').html('<font color="#FF0000">No project found</font>');
+                msgdiv.html('<font color="#FF0000">No project found</font>');
             }
+        }
+        var prevID = deserialize('release.relatedProjectID');
+        $("input[name='txtReleaseReleatedProject']").keyup(function(){
+            var val = $(this).val();
+            updateProjectName(val);
+        }).val(prevID);
+        if (prevID)
+            updateProjectName(prevID);
+        // notes
+        $('textarea#txtNotes').val(deserialize('release.note'))
+        // debug
+        console.log(localStorage);
+        // save result when submit
+        $('#form1').change(function() {
+            serialize('release.relatedProjectID', $("input[name='txtReleaseReleatedProject']").val());
+            serialize('release.note', $('textarea#txtNotes').val());
+            serialize('release.package', $('input#txtDeliveryPackage').val());
+            serialize('release.releasepath', $('input#txtReleasePath').val());
+            serialize('release.version', $("input[name='txtVersion']").val());
         });
-        // get release packages
-        //tds.css('background-color', 'red');
-        //console.log('hi');
     }
 }, true);
