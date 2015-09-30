3a1671100c5086c19a720538536a758b0fa15dd3
maxint <lnychina@gmail.com>
2015-09-30 09:11:39 +0800

    Add ArcSoft_Jenkins.user.js

---
+++ ArcSoft_Jenkins/ArcSoft_Jenkins.user.js
@@ -0,0 +1,111 @@
+// ==UserScript==
+// @name        ArcSoft Jenkins
+// @namespace   http://maxint.github.io
+// @description Save forms etc.
+// @include     http://abs02/jenkins/job/*/build*
+// @version     1
+// @grant       none
+// ==/UserScript==
+//
+
+// a function that loads jQuery and calls a callback function when jQuery has finished loading
+; (function (callback, safe) {
+    var callback2 = function (jQuery_old, jQuery) {
+        //Firefox supports
+        console.log('Using jquery ' + jQuery().jquery);
+        console.log('Runing custom script');
+        callback(jQuery_old, jQuery, typeof(unsafeWindow) == "undefined" ? window : unsafeWindow);
+    };
+    if (typeof(jQuery) == "undefined" || jQuery.jquery != '2.1.1') {
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "//apps.bdimg.com/libs/jquery/2.1.1/jquery.min.js";
+        if (safe) {
+            var cb = document.createElement("script");
+            cb.type = "text/javascript";
+            cb.textContent = "var jq = jQuery.noConflict();(" + callback.toString() + ")($, jq, window);";
+            script.addEventListener('load', function() {
+                document.head.appendChild(cb);
+            });
+        } else {
+            script.addEventListener('load', function () {
+                var jq = jQuery.noConflict();
+                callback2($, jq);
+            });
+        }
+        document.head.appendChild(script);
+    } else {
+        setTimeout(function () {
+            callback2(jQuery, jQuery);
+        }, 30);
+    }
+})(function (jq, $, window) {
+    // helper functions
+    // local storage
+    var IStorage = function (prefix) {
+        var pref = prefix + '.';
+        var addpref = function (key) { return pref + key; };
+        this.get = function (key, def) {
+            var val = window.localStorage.getItem(addpref(key));
+            if (val !== null)
+                return val;
+            else
+                return def || null;
+        };
+        this.set = function (key, val) {
+            window.localStorage.setItem(addpref(key), val);
+        };
+        this.flush = function () {
+            window.localStorage.clear();
+        };
+        this.getObject = function (key, def) {
+            var val = this.get(key);
+            return val ? JSON.parse(val) : (def || null);
+        };
+        this.setObject = function (key, val) {
+            this.set(key, JSON.stringify(val));
+        };
+    };
+
+    // assert function
+    function assert(condition, message) {
+        if (!condition) {
+            throw message || "Assertion failed";
+        }
+    }    // fill input values
+    function fillValues(key_id_pairs, istore) {
+        for (var key in key_id_pairs) {
+            if (key_id_pairs.hasOwnProperty(key)) {
+                var elem = key_id_pairs[key];
+                if (!elem.val())
+                    elem.val(istore.get(key, ''));
+            }
+        }
+    }
+    // save input values
+    function storeValues(key_id_pairs, istore) {
+        for (var key in key_id_pairs) {
+            if (key_id_pairs.hasOwnProperty(key)) {
+                istore.set(key, key_id_pairs[key].val());
+            }
+        }
+    }
+    // operate w.r.t. sub path
+    var subpath = window.location.pathname;
+    console.log(subpath);
+    if (/jenkins\/job\/[\w_]*\/build\b/.test(subpath)) {
+        var istore = new IStorage('project/build');
+        var inputNames = $('input[type="hidden"][name="name"]');
+        var inputValues = $('input.setting-input[name="value"]');
+        assert(inputNames.length == inputValues.length);
+        var pairs = {};
+        for (var i = 0; i < inputNames.length; ++i) {
+            pairs[inputNames[i].value] = $(inputValues[i]);
+        }
+        fillValues(pairs, istore);
+        $(window).unload(function () {
+            console.log('window unload');
+            storeValues(pairs, istore);
+        });
+    }
+}, true);
